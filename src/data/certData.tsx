d56d6402f908cc2b54239e297a17d5f2ff8059bc
lukewarmraven <lukewarmraven@gmail.com>
2026-01-20 02:52:38 +0800

    certificates mapped data, no styling yet

---
+++ src/data/certData.tsx
@@ -0,0 +1,90 @@
+export const certData = [
+    {
+      title: "Sample 1",
+      desc: "sample description 1",
+      thumb: "physics.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Trainocate GCP Fundamentals",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["physics.png",
+        "lambda.png",
+        "supabase.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    },
+    {
+      title: "Sample 2",
+      desc: "sample description 2",
+      thumb: "cloudfront.png",
+      images: ["crow.png",
+        "github.png",
+        "python.png"]
+    }
+]
--- src/pages/Certificates/CertificateView/CertificateView.tsx
+++ src/pages/Certificates/CertificateView/CertificateView.tsx
@@ -1,7 +1,28 @@
+import { useLocation } from "react-router-dom"
+import "./CertificateView.css"
 
 function CertificateView() {
+  const loc = useLocation()
+  const title = loc.state?.title
+  const desc = loc.state?.desc
+  const thumb = loc.state?.thumb
+  const images = loc.state?.images
+  console.log(images);
+  
+
   return (
-    <div>CertificateView</div>
+    <div className="certview-container">
+      <div className="certview-text certview-title">{title}</div>
+      <div className="certview-text">{desc}</div>
+      <div>
+        <img className="certview-img" src={`/src/assets/${thumb}`} />
+        {
+          images.map((v:any,i:number) => (
+            <img className="certview-img" key={i} src={`/src/assets/${v}`} />
+          ))
+        }
+      </div>
+    </div>
   )
 }
 
